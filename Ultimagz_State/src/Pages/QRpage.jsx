38b1e81f7aaa22991381a96412dd4f2cb6d11d87
Acipzz <moses.alexander@student.umn.ac.id>
2024-09-06 22:47:38 +0700

    3

--- Ultimagz_State/src/Pages/QRpage.jsx
+++ Ultimagz_State/src/Pages/QRpage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./QRpage.css";
+import bingkai from "./QRpage/campaign_front_asset_bingkai_n_kotak,png";
 
 function QRpage() {
   return (
     <div className="background">
       <div className="atas">
+        <img src={bingkai} alt="bingkai" />
               <p>Test</p>
       </div>
       <div className="bawah">
