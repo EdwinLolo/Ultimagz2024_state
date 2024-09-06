c4d43a34317bda6a8398e3a6e8d1a4ca2427254f
Acipzz <moses.alexander@student.umn.ac.id>
2024-09-06 22:30:57 +0700

    nama image

--- Ultimagz_State/src/Pages/QRpage.jsx
+++ Ultimagz_State/src/Pages/QRpage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+
 function QRpage() {
-  return <div>QRpage</div>;
+  return (
+    <div className="background">
+      <div className="atas">
+      <p>Test</p>
+      </div>
+      <div className="bawah">
+      <p>Test</p>
+      </div>
+    </div>
+  );
 }
 
 export default QRpage;
