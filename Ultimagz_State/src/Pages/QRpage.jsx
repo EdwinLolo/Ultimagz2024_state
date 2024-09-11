44db236d7823b62d93db3fdda6f840d16ff74dfc
owencahyadi <owenchristian123@gmail.com>
2024-09-11 15:51:53 +0700

    QR

--- Ultimagz_State/src/Pages/QRpage.jsx
+++ Ultimagz_State/src/Pages/QRpage.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./QRpage.css";
 import bingkai from "../assets/QRpage/bingkai.png";
 import awan from "../assets/QRpage/campaign_front_asset_clouds.png";
-import ground from "../assets/QRpage/ground.png";
+import groundBlkg from "../assets/QRpage/campaign_front_asset_ground_blkg.png";
+import groundDepan from "../assets/QRpage/campaign_front_asset_ground_depan.png";
+import Tas from "../assets/QRpage/campaign_front_asset_tas_ransel.png";
+import Book from "../assets/QRpage/campaign_front_asset_books.png"
 function QRpage() {
   return (
     <div className="background">
@@ -12,8 +15,17 @@ function QRpage() {
       <div className="awan">
         <img src={awan} alt="awan" />
       </div>
-      <div className="ground">
-        <img src={ground} alt="ground" />
+      <div className="groundDepan">
+        <img src={groundDepan} alt="ground" />
+      </div>
+      <div className="groundBlkg">
+        <img src={groundBlkg} alt="ground" />
+      </div>
+      <div className="Tas">
+        <img src={Tas} alt="ground" />
+      </div>
+      <div className="Book">
+        <img src={Book} alt="ground" />
       </div>
     </div>
   );
